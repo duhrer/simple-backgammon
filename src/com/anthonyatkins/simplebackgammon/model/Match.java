7187b3f735dc90e7a11a0675ac480196d59d266b
duhrer <duhrer@localhost>
2011-11-30 22:58:01 +0100

    Continued refactoring to implement database layer.  Now have CRUD methods for all objects (but without cascading at the moment).

--- src/com/anthonyatkins/simplebackgammon/model/Match.java
+++ src/com/anthonyatkins/simplebackgammon/model/Match.java
@@ -1,6 +1,8 @@
 package com.anthonyatkins.simplebackgammon.model;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 public class Match {
 	// Database setup information
@@ -38,7 +40,17 @@ public class Match {
 	private int numGames;
 	private boolean isFinished;
 	private final Date created = new Date();
+	private List<Game> games = new ArrayList<Game>();
 	
+	public void addGame(Game game) {
+		games.add(game);
+	}
+	public void deleteGame(Game game) {
+		games.remove(game);
+	}
+	public List<Game> getGames() {
+		return games;
+	}
 	public Date getCreated() {
 		return created;
 	}
